1d080fee1e3c1f8eb9f1ffe59c6ad813de9bd346
Lynne Ashminov <lashminov@gmail.com>
2015-10-27 14:28:15 -0400

    first set of features tests finished

--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -1,5 +1,6 @@
 class BoardGame < ActiveRecord::Base
   validates :name, presence: true
+  validates :name, uniqueness: { message: "This board game already exists in our records!" }
   validates :publisher, presence: true
   validates :description, presence: true
   validates :description, length: { minimum: 50 }
--- db/migrate/20151021162301_add_null_false_to_board_game_fields.rb
+++ db/migrate/20151021162301_add_null_false_to_board_game_fields.rb
@@ -1,4 +1,7 @@
 class AddNullFalseToBoardGameFields < ActiveRecord::Migration
   def change
+    change_column_null :board_games,
+    :description, false
+    change_column_null :board_games, :release_date, false
   end
 end
